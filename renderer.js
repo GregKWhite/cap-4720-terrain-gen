88aa38e6db5951ee71661bde789b3ebd8b598fdf
Greg White <gkwhite95@gmail.com>
2016-11-28 13:48:21 -0500

    Set up perspective rendering. sorta

--- renderer.js
+++ renderer.js
@@ -1,10 +1,14 @@
+renderState = {}
+
 function renderSphere() {
   var canvas = document.getElementById('myCanvas');
   var gl = canvas.getContext('webgl');
   gl.enable(gl.DEPTH_TEST);
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+  // Set up the initial state for the renderer
+  renderState.angle = 0;
+  renderState.rotateAngle = 0.01;
 
   var program = createProgram(gl, 'vertex-shader', 'fragment-shader');
   gl.useProgram(program);
@@ -12,7 +16,7 @@ function renderSphere() {
   // Set up attributes
   var positionLoc = gl.getAttribLocation(program, 'aPosition');
   var colorLoc = gl.getAttribLocation(program, 'aColor');
-  var matrixLoc = gl.getAttribLocation(program, 'aMatrix');
+  var matrixLoc = gl.getAttribLocation(program, 'uMatrix');
 
   // Set up uniforms
   var resolutionLoc = gl.getUniformLocation(program, 'uResolution');
@@ -27,7 +31,6 @@ function renderSphere() {
   // Set the geometry to use
   gl.bufferData(gl.ARRAY_BUFFER, initGeometry(), gl.STATIC_DRAW);
 
-
   // Initialize the webgl color buffer
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
@@ -37,7 +40,32 @@ function renderSphere() {
   // Set the colors to use
   gl.bufferData(gl.ARRAY_BUFFER, initColors(), gl.STATIC_DRAW);
 
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
+  // Draw the initial rectangles
+  drawScene(gl, canvas, program);
+  gl.drawArrays(gl.TRIANGLES, 0, 18);
+}
+
+function drawScene(gl, canvas, program) {
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  var angle = 60;
+
+  // Calculate matrices
+  var aspect = canvas.width / canvas.height;
+  var projectionMat = mat4.create();
+  var viewMat = mat4.create();
+  this.eye = [Math.sin(0.5) * 1.5, 0.5, Math.cos(0.01) * 1.5];
+
+  mat4.lookAt(viewMat, eye, [0,0,0], [0, 1.0, 0]);
+	mat4.perspective(projectionMat, degToRad(45), aspect, 0.1, 3);
+
+  var matrixLoc = gl.getUniformLocation(program, 'uMatrix');
+
+  gl.uniformMatrix4fv(matrixLoc, false, mat4.multiply(mat4.create(),projectionMat, viewMat));
+}
+
+function degToRad(deg) {
+  return deg * Math.PI / 180;
 }
 
 function initGeometry() {
@@ -45,7 +73,7 @@ function initGeometry() {
   colors = [];
 
   var width = 1;
-  var length = Math.sqrt(2);
+  var length = Math.sqrt(3);
 
   // Generate the initial rectangles used to
   // create the icosphere
@@ -71,16 +99,22 @@ function initGeometry() {
 
   var rectangle3 = p31.concat(p32, p33, p32, p33, p34);
 
-  return new Float32Array(rectangle1);
+  return new Float32Array(rectangle1.concat(rectangle2, rectangle3));
 }
 
 function initColors() {
+
+
+  return new Uint8Array(createColors().concat(createColors(), createColors()));
+}
+
+function createColors() {
   colors = [randColor(), randColor(), randColor(), randColor(), randColor(), randColor()]
   colors = colors.reduce(function(a, b) {
     return a.concat(b)
   });
 
-  return new Uint8Array(colors);
+  return colors;
 }
 
 function randColor() {
