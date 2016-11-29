8acb9184644514a6fa01ae509ba24dadf4c18197
Greg White <gkwhite95@gmail.com>
2016-11-28 20:59:35 -0500

    Add ability to subdivide n times

--- renderer.js
+++ renderer.js
@@ -52,8 +52,6 @@ function renderSphere() {
   gl.bufferData(gl.ARRAY_BUFFER, renderState.colors, gl.STATIC_DRAW);
 
   // Draw the initial rectangles
-  document.getElementById('animationToggle').addEventListener('click', toggleAnimation);
-
   requestAnimationFrame(drawScene);
 }
 
@@ -70,12 +68,12 @@ function drawScene(time) {
   var aspect = canvas.width / canvas.height;
   var projectionMat = mat4.create();
   var viewMat = mat4.create();
-  var rotationSpeed = 30;
+  var rotationSpeed = 20;
   rotation[2] += degToRad(timeDelta * rotationSpeed) % degToRad(360);
   rotation[1] += degToRad(timeDelta * rotationSpeed) % degToRad(360);
 
-  mat4.lookAt(viewMat, [0, 0, 2], [0,0,0], [0, 1.0, 0]);
-  mat4.rotate(viewMat, viewMat, degToRad(time * rotationSpeed), [1.0, 1, 0])
+  mat4.lookAt(viewMat, [0, 0, 5], [0,0,0], [0, 1, 0]);
+  mat4.rotate(viewMat, viewMat, degToRad(time * rotationSpeed), [0, 1, 0])
 	mat4.perspective(projectionMat, degToRad(60), aspect, 0.1, Math.sqrt(3) * 2000);
 
   var matrixLoc = gl.getUniformLocation(program, 'uMatrix');
@@ -86,11 +84,6 @@ function drawScene(time) {
   gl.drawArrays(gl.TRIANGLES, offset, renderState.geometry.length / 3 - offset);
 
   requestAnimationFrame(drawScene);
-}
-
-function toggleAnimation() {
-  console.log(animate);
-  animate = !animate;
 }
 
 function degToRad(deg) {
@@ -131,44 +124,45 @@ function initGeometry() {
 
   // Make the initial 20 triangles.
   var triangles = [
-    a2,a1,c1,
-    a2,c1,a3,
-    a2,a1,b3,
-    a2,b2,b3,
-    a2,b2,a3,
-    c3,a3,c1,
-    c3,a3,d1,
-    c3,c2,c1,
-    c3,d2,d1,
-    c3,d2,c2,
-    d3,b1,b3,
-    d3,b3,a1,
-    d3,d2,b1,
-    d3,d2,c2,
-    d3,a1,c2,
-    a1,c2,c1,
-    d2,b1,d1,
-    b2,a3,d1,
-    b2,d1,b1,
-    b2,b3,b1
+    new triangle(a2,a1,c1),
+    new triangle(a2,c1,a3),
+    new triangle(a2,a1,b3),
+    new triangle(a2,b2,b3),
+    new triangle(a2,b2,a3),
+    new triangle(c3,a3,c1),
+    new triangle(c3,a3,d1),
+    new triangle(c3,c2,c1),
+    new triangle(c3,d2,d1),
+    new triangle(c3,d2,c2),
+    new triangle(d3,b1,b3),
+    new triangle(d3,b3,a1),
+    new triangle(d3,d2,b1),
+    new triangle(d3,d2,c2),
+    new triangle(d3,a1,c2),
+    new triangle(a1,c2,c1),
+    new triangle(d2,b1,d1),
+    new triangle(b2,a3,d1),
+    new triangle(b2,d1,b1),
+    new triangle(b2,b3,b1)
   ]
 
-  for (var i = 0; i < triangles.length; i++) {
-    vertices = vertices.concat(triangles[i])
-  }
+  // for (var i = 0; i < triangles.length; i++) {
+  //   vertices = vertices.concat(triangles[i])
+  // }
 
   // Interpolate over each triangle
-  for (var i = 0; i < triangles.length / 3; i++) {
-    vertices = vertices.concat(
-      subdivide(
-        triangles[i * 3],
-        triangles[i * 3+ 1],
-        triangles[i * 3 + 2]
-      )
-    );
+  // for (var times = 0; times < 1; times++) {
+  //   vertices = subdivideTriangles(triangles)
+  // }
+
+  for (var times = 0; times < 3; times++) {
+    triangles = subdivideTriangles(triangles)
   }
 
-  // vertices = subdivideTriangles(vertices);
+  // Retrieve the actual coordinates from each triangle
+  for (var i = 0; i < triangles.length; i++) {
+    vertices = vertices.concat(triangles[i].p1, triangles[i].p2, triangles[i].p3)
+  }
 
   return new Float32Array(vertices);
 }
@@ -176,33 +170,33 @@ function initGeometry() {
 function subdivideTriangles(triangles) {
   var newTriangles = []
 
-  // // Interpolate over each triangle
-  for (var i = 0; i < triangles.length / 3; i++) {
-    newTriangles = newTriangles.concat(
-      subdivide(
-        triangles[i * 3],
-        triangles[i * 3+ 1],
-        triangles[i * 3 + 2]
-      )
-    );
+  // Interpolate over each triangle
+  for (var i = 0; i < triangles.length; i++) {
+    subdividedTriangles = subdivide(
+      triangles[i].p1,
+      triangles[i].p2,
+      triangles[i].p3
+    )
+
+    newTriangles = newTriangles.concat(subdividedTriangles)
   }
 
   return newTriangles;
 }
 
 function subdivide(point1, point2, point3) {
-  vertices = []
+  var triangles = []
 
-  m12 = normalizedMidpoint(point1, point2)
-  m13 = normalizedMidpoint(point1, point3)
-  m23 = normalizedMidpoint(point2, point3)
+  m1 = normalizedMidpoint(point1, point2)
+  m2 = normalizedMidpoint(point1, point3)
+  m3 = normalizedMidpoint(point2, point3)
 
-  vertices = vertices.concat(m12, m13, m23)
-  vertices = vertices.concat(point1, m12, m13)
-  vertices = vertices.concat(point2, m12, m23)
-  vertices = vertices.concat(point3, m13, m23)
+  triangles.push(new triangle(m1, m2, m3))
+  triangles.push(new triangle(point1, m1, m2))
+  triangles.push(new triangle(m1, point2, m3))
+  triangles.push(new triangle(m2, m3, point3))
 
-  return vertices;
+  return triangles;
 }
 
 function normalizedMidpoint(point1, point2) {
@@ -212,16 +206,11 @@ function normalizedMidpoint(point1, point2) {
     (point1[2] + point2[2]) / 2
   ]
 
-  console.log("p1: " + point1 + ", p2: " + point2 + ", mid: " + midpoint)
-  console.log(point1[1] + " + " + point2[1] + " / 2 = " + midpoint[1]);
-
   radius = Math.sqrt(
     midpoint[0] * midpoint[0] +
     midpoint[1] * midpoint[1] +
     midpoint[2] * midpoint[2]
   )
-
-  // console.log(radius)
 
   return [
     midpoint[0] / radius,
@@ -311,4 +300,10 @@ function getShaderType(gl, type) {
   }
 
   throw("Shader type not set for " + shaderId);
+}
+
+function triangle(p1, p2, p3) {
+  this.p1 = p1
+  this.p2 = p2
+  this.p3 = p3
 }
