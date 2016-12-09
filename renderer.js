05a2efa1501d778507dab2fb83bfeb65558e0186
Greg White <gkwhite95@gmail.com>
2016-12-08 23:36:48 -0500

    Makes code far more object-oriented

--- renderer.js
+++ renderer.js
@@ -12,7 +12,6 @@ function renderSphere() {
   canvas = document.getElementById('myCanvas');
   gl = canvas.getContext('webgl');
   gl.enable(gl.DEPTH_TEST);
-  gl.clearColor(0.7, 0.7, 0.7, 1.0);
   gl.clearColor(0, 0, 0, 1);
 
   // Set up the initial state for the renderer
@@ -176,55 +175,55 @@ function initGeometry() {
   colors = [];
 
   var width = 1;
-  var length = Math.sqrt(3);
+  var length = 1.618033988749895; // Golden ratio
 
   // Generate the initial rectangles used to
   // create the icosphere
-  var a1 = [-length/2, width/2, 0.0];
-  var b1 = [length/2, width/2, 0.0];
-  var c1 = [-length/2, -width/2, 0.0];
-  var d1 = [length/2, -width/2, 0.0];
+  var a1 = new Point(-length/2, width/2, 0.0);
+  var b1 = new Point(length/2, width/2, 0.0);
+  var c1 = new Point(-length/2, -width/2, 0.0);
+  var d1 = new Point(length/2, -width/2, 0.0);
 
-  var a2 = [-width/2, 0.0, length/2];
-  var b2 = [width/2, 0.0, length/2];
-  var c2 = [-width/2, 0.0, -length/2];
-  var d2 = [width/2, 0.0, -length/2];
+  var a2 = new Point(-width/2, 0.0, length/2);
+  var b2 = new Point(width/2, 0.0, length/2);
+  var c2 = new Point(-width/2, 0.0, -length/2);
+  var d2 = new Point(width/2, 0.0, -length/2);
 
-  var a3 = [0.0, -length/2, width/2];
-  var b3 = [0.0, length/2, width/2];
-  var c3 = [0.0, -length/2, -width/2];
-  var d3 = [0.0, length/2, -width/2];
+  var a3 = new Point(0.0, -length/2, width/2);
+  var b3 = new Point(0.0, length/2, width/2);
+  var c3 = new Point(0.0, -length/2, -width/2);
+  var d3 = new Point(0.0, length/2, -width/2);
 
 
   // Make the initial 20 triangles.
   var triangles = [
-    new triangle(a2,a1,c1),
-    new triangle(a2,c1,a3),
-    new triangle(a2,a1,b3),
-    new triangle(a2,b2,b3),
-    new triangle(a2,b2,a3),
-    new triangle(c3,a3,c1),
-    new triangle(c3,a3,d1),
-    new triangle(c3,c2,c1),
-    new triangle(c3,d2,d1),
-    new triangle(c3,d2,c2),
-    new triangle(d3,b1,b3),
-    new triangle(d3,b3,a1),
-    new triangle(d3,d2,b1),
-    new triangle(d3,d2,c2),
-    new triangle(d3,a1,c2),
-    new triangle(a1,c2,c1),
-    new triangle(d2,b1,d1),
-    new triangle(b2,a3,d1),
-    new triangle(b2,d1,b1),
-    new triangle(b2,b3,b1)
+    new Triangle(a2,a1,c1),
+    new Triangle(a2,c1,a3),
+    new Triangle(a2,a1,b3),
+    new Triangle(a2,b2,b3),
+    new Triangle(a2,b2,a3),
+    new Triangle(c3,a3,c1),
+    new Triangle(c3,a3,d1),
+    new Triangle(c3,c2,c1),
+    new Triangle(c3,d2,d1),
+    new Triangle(c3,d2,c2),
+    new Triangle(d3,b1,b3),
+    new Triangle(d3,b3,a1),
+    new Triangle(d3,d2,b1),
+    new Triangle(d3,d2,c2),
+    new Triangle(d3,a1,c2),
+    new Triangle(a1,c2,c1),
+    new Triangle(d2,b1,d1),
+    new Triangle(b2,a3,d1),
+    new Triangle(b2,d1,b1),
+    new Triangle(b2,b3,b1)
   ]
 
   for (var i = 0; i < triangles.length; i++) {
-    triangles[i] = new triangle(
-      normalize(triangles[i].p1),
-      normalize(triangles[i].p2),
-      normalize(triangles[i].p3)
+    triangles[i] = new Triangle(
+      triangles[i].p1.normalize(),
+      triangles[i].p2.normalize(),
+      triangles[i].p3.normalize()
     );
   }
 
@@ -235,7 +234,11 @@ function initGeometry() {
 
   // Retrieve the actual coordinates from each triangle
   for (var i = 0; i < triangles.length; i++) {
-    vertices = vertices.concat(triangles[i].p1, triangles[i].p2, triangles[i].p3)
+    vertices = vertices.concat(
+      triangles[i].p1.toArray(),
+      triangles[i].p2.toArray(),
+      triangles[i].p3.toArray()
+    )
   }
 
   return new Float32Array(vertices);
@@ -246,54 +249,24 @@ function subdivideTriangles(triangles) {
 
   // Interpolate over each triangle
   for (var i = 0; i < triangles.length; i++) {
-    subdividedTriangles = subdivide(
-      triangles[i].p1,
-      triangles[i].p2,
-      triangles[i].p3
+    newTriangles = newTriangles.concat(
+      subdivide(triangles[i])
     )
-
-    newTriangles = newTriangles.concat(subdividedTriangles)
   }
 
   return newTriangles;
 }
 
-function subdivide(point1, point2, point3) {
-  var triangles = []
-
-  m1 = normalizedMidpoint(point1, point2)
-  m2 = normalizedMidpoint(point1, point3)
-  m3 = normalizedMidpoint(point2, point3)
-
-  triangles.push(new triangle(m1, m2, m3))
-  triangles.push(new triangle(point1, m1, m2))
-  triangles.push(new triangle(m1, point2, m3))
-  triangles.push(new triangle(m2, m3, point3))
-
-  return triangles;
-}
-
-function normalizedMidpoint(point1, point2) {
-  midpoint = [
-    (point1[0] + point2[0]) / 2,
-    (point1[1] + point2[1]) / 2,
-    (point1[2] + point2[2]) / 2
-  ]
-
-  return normalize(midpoint);
-}
-
-function normalize(points) {
-  magnitude = Math.sqrt(
-    points[0] * points[0] +
-    points[1] * points[1] +
-    points[2] * points[2]
-  )
+function subdivide(triangle) {
+  m1 = triangle.p1.midpoint(triangle.p2).normalize();
+  m2 = triangle.p1.midpoint(triangle.p3).normalize();
+  m3 = triangle.p2.midpoint(triangle.p3).normalize();
 
   return [
-    points[0] / magnitude,
-    points[1] / magnitude,
-    points[2] / magnitude
+    new Triangle(m1, m2, m3),
+    new Triangle(triangle.p1, m1, m2),
+    new Triangle(m1, triangle.p2, m3),
+    new Triangle(m2, m3, triangle.p3)
   ]
 }
 
@@ -315,7 +288,7 @@ function createAndLinkProgram(gl, vertexShader, fragmentShader) {
   gl.linkProgram(program);
 
   if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-     throw("program failed to link: " + gl.getProgramInfoLog(program));
+     throw("Program failed to link :( " + gl.getProgramInfoLog(program));
   }
 
   return program;
@@ -324,14 +297,12 @@ function createAndLinkProgram(gl, vertexShader, fragmentShader) {
 function createShader(gl, shaderId) {
   var shaderScript = document.getElementById(shaderId);
 
-  if (!shaderScript) { throw("Script with id " + shaderId + " could not be found.") }
-
   var shader = gl.createShader(getShaderType(gl, shaderScript.type));
   gl.shaderSource(shader, shaderScript.text);
   gl.compileShader(shader);
 
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    throw("could not compile shader: " + gl.getShaderInfoLog(shader));
+    throw("Shader compilation error: " + gl.getShaderInfoLog(shader));
   }
 
   return shader;
@@ -344,11 +315,55 @@ function getShaderType(gl, type) {
     return gl.FRAGMENT_SHADER;
   }
 
-  throw("Shader type not set for " + shaderId);
+  throw(type + " is not a valid shader type.")
 }
 
-function triangle(p1, p2, p3) {
+function Triangle(p1, p2, p3) {
   this.p1 = p1
   this.p2 = p2
   this.p3 = p3
+}
+
+function Point(v1, v2, v3) {
+  this.v1 = v1
+  this.v2 = v2
+  this.v3 = v3
+
+  this.normalize = function() {
+    magnitude = Math.sqrt(this.v1 * this.v1 + this.v2 * this.v2 + this.v3 * this.v3);
+
+    return new Point(
+      this.v1 / magnitude,
+      this.v2 / magnitude,
+      this.v3 / magnitude
+    );
+  }
+
+  this.add = function(point2) {
+    return new Point(
+      this.v1 + point2.v1,
+      this.v2 + point2.v2,
+      this.v3 + point2.v3
+    );
+  }
+
+  this.divide = function(value) {
+    return new Point(
+      this.v1 / value,
+      this.v2 / value,
+      this.v3 / value
+    );
+  }
+
+  this.toArray = function() {
+    return [
+      this.v1,
+      this.v2,
+      this.v3
+    ];
+  }
+
+  this.midpoint = function(point2) {
+    return this.add(point2).divide(2);
+  }
 }
